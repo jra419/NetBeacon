4fec2e6e624370bc24186d15736fcaedde2b077d
jra <joao.amado@tecnico.ulisboa.pt>
2024-05-21 11:03:41 +0100

    Save .dot models in separate dirs.

--- model_generation/rf_train.py
+++ model_generation/rf_train.py
@@ -1,21 +1,52 @@
+import os
 import sys
 import argparse
 import json
 import statistics
 import pandas as pd
+from pathlib import Path
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import export_graphviz
 
 
-def fc_process(train_file, inf_point):
-    print(f'Processing the input json with inference point: {inf_point}')
+def fc_pkt(train_file):
+    print('Processing input features (packet-level).')
+
+    fc_pkts = []
+
+    with open(train_file, 'r') as tf:
+        json_data = json.load(tf)
+
+        for data in json_data:
+            pkt_num = data['packet_num']
+            proto = data['proto']
+
+            for pkt in range(pkt_num):
+                pkt_len = data['len_seq'][pkt]
+                pkt_diffserv = data['ip_tos_seq'][pkt]
+                pkt_ttl = data['ip_ttl_seq'][pkt]
+                pkt_tcp_off = data['tcp_off_seq'][pkt]
+                pkt_tcp_win = data['tcp_win_seq'][pkt]
+                pkt_udp_len = data['udp_len_seq'][pkt]
+
+                fc_pkts.append([data['source'], pkt, proto, pkt_len, pkt_diffserv, pkt_ttl,
+                                pkt_tcp_off, pkt_tcp_win, pkt_udp_len, str(data['label'])])
+
+    df_pkts = pd.DataFrame(fc_pkts, columns = ['source', 'pkt_index', 'protocol', 'total_len',
+                                               'diffserv', 'ttl', 'tcp_offset', 'tcp_win',
+                                               'udp_len', 'label'])
+
+    return df_pkts
+
+def fc_flow(train_file, inf_point):
+    print(f'Processing input features (flow-level). Inference point: {inf_point}')
 
     fc_flows = []
 
     with open(train_file, 'r') as tf:
         json_data = json.load(tf)
 
-        for data in json_data:
+        for idx, data in enumerate(json_data):
             pkt_num = data['packet_num']
             if pkt_num < inf_point:
                 continue
@@ -37,22 +68,45 @@ def fc_process(train_file, inf_point):
                              pkt_size_variance, flow_iat_min, str(data['label'])])
 
     df_flows = pd.DataFrame(fc_flows, columns = ['source', 'pkt_size_max', 'pkt_size_min',
-                                                    'pkt_size_mean', 'pkt_size_variance',
-                                                    'flow_iat_min', 'label'])
+                                                'pkt_size_mean', 'pkt_size_variance', 'flow_iat_min',
+                                                'label'])
 
     return df_flows
 
-def train(df_flows, inf_point, export_graph):
-    print(f'RF model training with inference point: {inf_point}')
+def train_pkts(df_pkts, export_graph, train_name):
+    print('Packet-level: RF model training')
+
+    df_features = df_pkts.iloc[:, [2, 3, 4, 5, 6, 7, 8]]
+    rfc = RandomForestClassifier(n_estimators=3, max_depth=7)
+    rfc.fit(df_features, df_pkts['label'])
+
+    outdir = str(Path(__file__).parents[0]) + '/models/' + train_name
+    if not os.path.exists(str(Path(__file__).parents[0]) + '/models/' + train_name):
+        os.mkdir(outdir)
+
+    if export_graph:
+        for i in range(len(rfc.estimators_)):
+            export_graphviz(rfc.estimators_[i],
+                            out_file = outdir + '/rf_tree_pkt_{}.dot'.format(i),
+                            feature_names = df_features.columns,
+                            class_names = df_pkts['label'].values,
+                            rounded = True, proportion = False, precision = 4, filled = True)
+
+def train_flows(df_flows, inf_point, export_graph, train_name):
+    print(f'Flow-level: RF model training with inference point: {inf_point}')
 
     df_features = df_flows.iloc[:, [1, 2, 3, 4, 5]]
     rfc = RandomForestClassifier(n_estimators=3, max_depth=7)
     rfc.fit(df_features, df_flows['label'])
 
+    outdir = str(Path(__file__).parents[0]) + '/models/' + train_name
+    if not os.path.exists(str(Path(__file__).parents[0]) + '/models/' + train_name):
+        os.mkdir(outdir)
+
     if export_graph:
         for i in range(len(rfc.estimators_)):
             export_graphviz(rfc.estimators_[i],
-                            out_file='rf_tree_' + inf_point + '_{}.dot'.format(i),
+                            out_file = outdir + '/rf_tree_flow_' + inf_point + '_{}.dot'.format(i),
                             feature_names = df_features.columns,
                             class_names = df_flows['label'].values,
                             rounded = True, proportion = False, precision = 4, filled = True)
@@ -60,19 +114,24 @@ def train(df_flows, inf_point, export_graph):
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description='RF training.')
     argparser.add_argument('--train_file', type=str, help='Train file path')
+    argparser.add_argument('--train_name', type=str, help='Dataset/Trace name')
     argparser.add_argument('--graph', type=bool, default=False, help='Export RF model as dot graph')
     args = argparser.parse_args()
 
-    # Process the input json for the various inference points.
-    df_flows_8      = fc_process(args.train_file, 8)
-    df_flows_32     = fc_process(args.train_file, 32)
-    df_flows_256    = fc_process(args.train_file, 256)
-    df_flows_512    = fc_process(args.train_file, 512)
-    df_flows_2048   = fc_process(args.train_file, 2048)
+    # Pkt-level: Process the input json.
+    df_pkts         = fc_pkt(args.train_file)
+
+    # Flow-level: Process the input json for the various inference points.
+    df_flows_8      = fc_flow(args.train_file, 8)
+    df_flows_32     = fc_flow(args.train_file, 32)
+    df_flows_256    = fc_flow(args.train_file, 256)
+    df_flows_512    = fc_flow(args.train_file, 512)
+    df_flows_2048   = fc_flow(args.train_file, 2048)
 
     # Training: RF.
-    train(df_flows_8, '8', args.graph)
-    train(df_flows_32, '32', args.graph)
-    train(df_flows_256, '256', args.graph)
-    train(df_flows_512, '512', args.graph)
-    train(df_flows_2048, '2048', args.graph)
+    train_pkts(df_pkts, args.graph, args.train_name)
+    train_flows(df_flows_8, '8', args.graph, args.train_name)
+    train_flows(df_flows_32, '32', args.graph, args.train_name)
+    train_flows(df_flows_256, '256', args.graph, args.train_name)
+    train_flows(df_flows_512, '512', args.graph, args.train_name)
+    train_flows(df_flows_2048, '2048', args.graph, args.train_name)
